7d8e0bcdf58f1569f68ac532a75bb76447a3a892
wintjade <winterxuc@gmail.com>
2021-07-06 13:17:27 +0800

    update

--- src/jd_joy-park.js
+++ src/jd_joy-park.js
@@ -1,9 +1,9 @@
 /*
 入口 极速版 旺财乐园
-分享到QQ查看邀请码 inviter就是
-或者运行脚本查看邀请码
+新用户第一次想设置主号拉邀请就先分享到QQ查看邀请码 inviter就是
+运行脚本查看邀请码
 #自定义邀请码变量
-export inviterPin="" //IANWqUmbgQVF9ePHGsGFA2m-zSTLKmHFbE-IW-Waarw
+export joyinviterPin="" //IANWqUmbgQVF9ePHGsGFA2m-zSTLKmHFbE-IW-Waarw
 
 ====================
 新手上路勿黑勿喷
@@ -12,7 +12,7 @@ TG通知群 https://t.me/ningmeng999
 */
 // [task_local]
 // #柠檬旺财乐园
-// 0 */1 * * *
+// */5 * * * *
 
 const $ = new Env('柠檬旺财乐园新手上路版');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -21,13 +21,13 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let tytpacketId = '';
+
 let codeList = []
 let codeList1 = []
 
-let inviterPin = '';
-if (process.env.inviterPin) {
-  inviterPin = process.env.inviterPin;
+let joyinviterPin = '';
+if (process.env.joyinviterPin) {
+  joyinviterPin = process.env.joyinviterPin;
 }
 
 
@@ -395,7 +395,8 @@ headers: {
                        $.log(`\n===============开地邀请===============`)
 
                        await openinvite()
-
+                       //$.log(`\n===============升级奖励===============`)
+                       //await levelDrawAward()
 
                     console.log("操作失败")
 
@@ -414,7 +415,7 @@ function openinvite() {
                 let options = {
     url: `https://api.m.jd.com/`,
 
-    body: `functionId=joyBaseInfo&body={"taskId":"","inviteType":"2","inviterPin":"${inviterPin}","linkId":"LsQNxL7iWDlXUs6cFl-AAg"}&_t=1625540360946&appid=activities_platform`,
+    body: `functionId=joyBaseInfo&body={"taskId":"","inviteType":"2","inviterPin":"${joyinviterPin}","linkId":"LsQNxL7iWDlXUs6cFl-AAg"}&_t=1625540360946&appid=activities_platform`,
 headers: {
 "Origin": "https://joypark.jd.com",
 "Host": "api.m.jd.com",
@@ -457,7 +458,7 @@ function inviteType() {
                 let options = {
     url: `https://api.m.jd.com/`,
 
-    body: `functionId=joyBaseInfo&body={"taskId":"167","inviteType":"1","inviterPin":"${inviterPin}","linkId":"LsQNxL7iWDlXUs6cFl-AAg"}&_t=1625540360946&appid=activities_platform`,
+    body: `functionId=joyBaseInfo&body={"taskId":"167","inviteType":"1","inviterPin":"${joyinviterPin}","linkId":"LsQNxL7iWDlXUs6cFl-AAg"}&_t=1625540360946&appid=activities_platform`,
 headers: {
 "Origin": "https://joypark.jd.com",
 "Host": "api.m.jd.com",
@@ -484,6 +485,49 @@ headers: {
                 }else  if(data.success == false){
 
                     console.log(data.errMsg+"或者你的CK不足")
+
+                }
+            } catch (e) {
+                $.logErr(e, resp);
+            } finally {
+                resolve();
+            }
+        });
+    });
+}
+function levelDrawAward() {
+    return new Promise(async (resolve) => {
+
+                let options = {
+    url: `https://api.m.jd.com/`,
+
+    body: `functionId=joyBaseInfo&body={"taskId":"167","inviteType":"1","inviterPin":"${yqm}","linkId":"LsQNxL7iWDlXUs6cFl-AAg"}&_t=1625545015696&appid=activities_platform`,
+headers: {
+"Origin": "https://joypark.jd.com",
+"Host": "api.m.jd.com",
+"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 UBrowser/5.6.12150.8 Safari/537.36",
+      "Cookie": cookie,
+      }
+                }
+
+        $.post(options, async (err, resp, data) => {
+             //$.log(data)
+            try {
+
+                    data = JSON.parse(data);
+
+
+
+                    if(data.success == true){
+
+
+                    $.log(data.errMsg)
+
+
+
+                }else  if(data.success == false){
+
+                    console.log(data.errMsg)
 
                 }
             } catch (e) {
